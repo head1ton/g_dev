28de44ca268320bf466739c0e81e35cd983d9ec9
Hwan <head1ton@gmail.com>
2025-07-26 23:07:49 +0900

    create method : ReadFile

--- internal/utils/file_processor.go
+++ internal/utils/file_processor.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
 // 파일 작업 결과를 저장하는 구조체
 type FileOperation struct {
 	Operation string `json:"operation"` // 수행된 작업 (read, write, list, etc)
@@ -27,4 +33,68 @@ func NewFileProcessor(workingDir string) *FileProcessor {
 		WorkingDir: workingDir,
 		History:    make([]FileOperation, 0),
 	}
+}
+
+// 지정된 파일을 읽어서 내용을 반환. 파일이 존재하지 않거나 읽을 수 없는 경우 에러 반환
+func (fp *FileProcessor) ReadFile(filename string) ([]byte, error) {
+	// 절대 경로가 아니면 작업 디렉토리와 결합
+	if !filepath.IsAbs(filename) {
+		filename = filepath.Join(fp.WorkingDir, filename)
+	}
+
+	// 파일 열기
+	file, err := os.Open(filename)
+	if err != nil {
+		// 에러 발생 시 히스토리에 기록
+		op := FileOperation{
+			Operation: "read",
+			Path:      filename,
+			Success:   false,
+			Error:     err.Error(),
+			Size:      0,
+		}
+		fp.History = append(fp.History, op)
+		return nil, err
+	}
+	defer file.Close()
+
+	// 파일 정보 가져오기
+	fileInfo, err := file.Stat()
+	if err != nil {
+		op := FileOperation{
+			Operation: "read",
+			Path:      filename,
+			Success:   false,
+			Error:     err.Error(),
+			Size:      0,
+		}
+		fp.History = append(fp.History, op)
+		return nil, err
+	}
+
+	// 파일 내용 읽기
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		op := FileOperation{
+			Operation: "read",
+			Path:      filename,
+			Success:   false,
+			Error:     err.Error(),
+			Size:      0,
+		}
+		fp.History = append(fp.History, op)
+		return nil, err
+	}
+
+	// 성공 시 히스토리 기록
+	op := FileOperation{
+		Operation: "read",
+		Path:      filename,
+		Success:   true,
+		Error:     "",
+		Size:      fileInfo.Size(),
+	}
+	fp.History = append(fp.History, op)
+
+	return data, nil
 }
